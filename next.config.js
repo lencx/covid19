b86d4f24107197a42825f1f63c02199cded8dad4
lencx <cxin1314@gmail.com>
2020-03-16 00:25:29 +0800

    chore: add husky

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  webpack(config, options) {
+  webpack(config) {
     config.resolve.alias['~'] = path.resolve(__dirname, 'src');
     return config;
   },
